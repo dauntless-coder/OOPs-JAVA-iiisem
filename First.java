8887f36a7f56873993ea87d9f7643176d22961bd
Aditi Sharma <aditisharma9104@gmail.com>
2025-08-05 09:56:49 +0530

    5/8/25

---
+++ First.java
@@ -0,0 +1,10 @@
+public class First {
+    // print your name 01
+    // print your address 02
+    public static void main(String[] args){
+        System.out.println("Aditi Sharma ");
+        System.out.println("CET girls hostel- 101");
+
+
+    }
+}

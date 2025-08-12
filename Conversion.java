464da97ef609730e04b4b6fe60cb472ff3984e62
Aditi <Aditi@dauntlesscoder.com>
2025-08-12 09:13:03 +0530

    updated

---
+++ Conversion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Conversion {
+    // read a number in metre and convert to feet and display the result
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter distance in metres: ");
+        double metres = sc.nextDouble();
+
+        // Conversion factor: 1 metre = 3.28084 feet
+        double feet = metres * 3.28084;
+
+        System.out.printf(" %.2f metres is equal to %.2f feet.%n", metres, feet);
+
+
+    }
+}
